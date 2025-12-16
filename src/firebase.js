eec118918f03cfce209897c0e5379e44a74d0376
Abdurrehman994 <rehmance39b@gmail.com>
2025-12-16 19:14:46 +0200

    changed account

--- src/firebase.js
+++ src/firebase.js
@@ -6,9 +6,9 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "457400027952",
-  appId: "1:457400027952:web:4ca9056da24f02392c56f5",
-  measurementId: "G-R5S2WKB7J4",
+  messagingSenderId: "703160455754",
+  appId: "1:703160455754:web:f82202606dab70cfa68cb3",
+  measurementId: "G-63GF8NFXE6",
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
